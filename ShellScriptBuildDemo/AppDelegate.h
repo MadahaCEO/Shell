e8bfb10f5b67672b3ecd755fb8fcf37fd999e907
马大哈 <madahaceo@163.com>
2018-01-08 20:45:45 +0800

    创建工程

---
+++ ShellScriptBuildDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShellScriptBuildDemo
+//
+//  Created by Apple on 2017/11/8.
+//  Copyright © 2017年 马大哈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
